beb3ab5ed80bc861adae4e05c1633fca5ce68075
mehkpentester <mehkpentester>
2022-09-01 15:42:37 +0630

    This is if else and else if program

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-let tired=prompt(`Are you tired? "yes or no"`);
-let sms = "smart worker";
+// if else example 
+//gender check and fill full word
 
-if(tired==="yes"){
-    console.log("Rest will " + sms);
-}else if(tired==="no"){
-    console.log("GO back to work "+ sms );
-}else
-console.log("Pls enter yes or no " + sms);
+let people = [
+    {name:'john',age:30},
+    {name:'Jane',age:20,gender:'f'},
+    {name:'Jack',age:40,gender:'m'}
+]
+//if else coding
+/* people=people.map(person=>{
+    if(person.gender){
+        if(person.gender==='m')
+            person.gender="male"
+        if(person.gender==='f')
+            person.gender="female"
+    }
+    else
+        person.gender="unknown"
+    return person
+})
+console.log(people); */
+
+//with else if
+
+people=people.map(person=>{
+    if(person.gender==='m')
+        person.gender="male"
+    else if(person.gender==="f")
+        person.gender="female"
+    else
+        person.gender="unknown"
+    return person
+})
+console.log(people);
