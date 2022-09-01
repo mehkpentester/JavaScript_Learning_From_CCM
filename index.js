c477f20397adf4e784f552dc93dbc298acc90416
mehkpentester <mehkpentester>
2022-09-01 11:06:05 +0630

    if else

---
+++ index.js
@@ -0,0 +1,11 @@
+let tired=prompt(`are you tired? "yes/no"`);
+let sms = ":):):)Thank You:):):)"
+if (tired==="yes"){
+    console.log("rest will " +sms);
+}else if(tired==="no"){
+    console.log("Go back to work "+sms);
+}else{
+    console.log("Please enter yes or no " +sms);
+}
+
+    
