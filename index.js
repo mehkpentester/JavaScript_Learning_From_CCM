abec46351815591e02de48332c0f7197056cf9a5
mehkpentester <mehkpentester>
2022-09-01 11:23:34 +0630

    This is if else

---
+++ index.js
@@ -0,0 +1,9 @@
+let tired=prompt(`Are you tired? "yes or no"`);
+let sms = "smart worker";
+
+if(tired==="yes"){
+    console.log("Rest will " + sms);
+}else if(tired==="no"){
+    console.log("GO back to work "+ sms );
+}else
+console.log("Pls enter yes or no " + sms);
