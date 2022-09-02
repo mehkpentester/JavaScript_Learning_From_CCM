1bd93db7dadd100f3604ac67435adab8201dd875
mehkpentester <mehkpentester>
2022-09-02 20:27:20 +0630

    this is exercise

---
+++ for_of_loop.js
@@ -0,0 +1,17 @@
+//for of loop //using loop in array
+
+// let people = ['aungaung','mgmg','kyawkyaw']
+
+// for(person of people){
+//     console.log(person)
+// }
+
+//for in loop //using loop in object
+
+let person = {
+    name:'kyawkhaing',
+    age:20
+}
+
+for (key in person){
+    console.log(`${key} is ${person[key]}`)}
---
+++ scoping.js
@@ -0,0 +1,10 @@
+//scoping js
+
+let x=0; //global variable
+console.log(x)
+
+{
+    let y=10; //local scope local variable
+    
+}
+console.log(y)
